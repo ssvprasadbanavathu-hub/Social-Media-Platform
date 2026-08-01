774b14287ce158e7df066ee092db34808a7116f7
ssvprasadbanavathu-hub <ssvprasadbanavathu@gmail.com>
2026-08-01 19:06:44 +0530

    Set DEBUG to False

--- social_media/settings.py
+++ social_media/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-myfriend-social-platform-secret-key-production'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
